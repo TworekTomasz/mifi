d5bc96691302defe016f1ee84341d2388d6447c9
Tomasz Tworek <tomasztworek27@gmail.com>
2025-09-25 13:55:28 +0200

    Added prod conf

--- mifi-backend/src/main/java/pl/mifi/buget/api/BudgetController.java
+++ mifi-backend/src/main/java/pl/mifi/buget/api/BudgetController.java
@@ -4,7 +4,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.mifi.buget.application.CreateBudgetCommandHandler;
 import pl.mifi.buget.application.GetByMonthQuery;
-import pl.mifi.buget.application.GetByMonthQueryHandler;
 import pl.mifi.buget.domain.Budget;
 import pl.mifi.cqrs.Mediator;
 
